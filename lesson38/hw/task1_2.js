9a07aa2606d6dc0ea393db2cc13b3cfd7dba0299
Yaroslav Benin <dondublon1@yandex.ru>
2025-12-20 12:04:40 +0200

    hw 38, task 1-2 finished.

--- lesson38/hw/task1_2.js
+++ lesson38/hw/task1_2.js
@@ -1,11 +1,20 @@
-let arr = [1, "rrr", 2, "qqq"];
+let arr = [1, "rrr", 2, "qqq", ["wie", 345, "gfjk"]];
 
 console.log(gatherStrings(arr));
 
 function gatherStrings(arr_) {
     let arr_new = [];
     for (let i = 0; i < arr_.length; i++) {
-        if (typeof arr_[i] === "string") {
+        // console.log(typeof arr_[i]);
+        if (Array.isArray(arr_[i])) {
+            // console.log('got array')
+            for (let i2 = 0; i2 < arr_[i].length; i2++) {
+                if (typeof arr_[i][i2] === "string") {
+                    arr_new.push(arr_[i][i2]);
+                }
+            }
+        }
+        else if (typeof arr_[i] === "string") {
             arr_new.push(arr_[i]);
         }
     }
