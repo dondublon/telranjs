69a5f3e972fcda64c160c002bdd1b75ca11311d6
Yaroslav Benin <dondublon1@yandex.ru>
2025-12-20 12:17:46 +0200

    hw 38, almost done

--- lesson38/hw/task3.js
+++ lesson38/hw/task3.js
@@ -1,1 +1,20 @@
-const num = prompt("Please enter a number")
+const num_s = prompt("Please enter a number");
+let num = Number(num_s);
+console.log(num);
+
+if (isNaN(num))
+    console.error("Wrong value")
+else {
+    let arr = toArray(num);
+    console.log(arr);
+}
+
+function toArray(num) {
+    let s = String(num);
+    let arr = [];
+    for (let i = 0; i < s.length; i++) {
+        console.log(s[i]);
+        arr.push(Number(s[i]));
+    }
+    return arr;
+}
