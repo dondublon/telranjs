a664706ab8720e7b4887c755d1fd77b28a5c72a8
Yaroslav Benin <dondublon1@yandex.ru>
2025-12-28 14:37:54 +0200

    hw 40,  task 2.3

--- lesson40/task2.js
+++ lesson40/task2.js
@@ -24,6 +24,15 @@ function Account(iban, owner, balance) {
             return false;
         }
     }
+    this.withdraw2 = function (amount) {
+        // null = success, string = error
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            return null;
+        } else {
+            return "Not enough money";
+        }
+    }
     this.getBalance = function () {
         return this.balance;
     }
@@ -57,3 +66,50 @@ printAccounts(accounts);
 
 transfer(account1, account2, 60);
 printAccounts(accounts);
+
+/*
+с) (чуть сложнее****************)
+В качестве результата функции transaer, в случае успешной
+операции, должен cформироваться объект:
+    - account1 (счет списания),
+- account2 (счет зачисления),
+- amount (сумма)
+- transactionInfo() (функция, которая выводит информацию о транзакции)
+
+Если транзакция прошла неуспешно, объект должен содержать
+еще и поле error c информацией об ошибке. Естественно,
+    transactionInfo() должна в этом случае выводить информацию
+о неуспешной транзакции. В случае, если транзакция успешна,
+    поля error не должно быть.
+
+*/
+function Transaction(acc1, acc2, amount, error=null) {
+    this.account1 = acc1;
+    this.account2 = acc2;
+    this.amount = amount;
+    this.error = error;
+    this.transactionInfo = function () {
+        if (this.error == null) {
+            return `Transfer from ${this.account1};  Tp ${this.account2}; Amount ${this.amount}`;
+        }
+        else {
+            return `Error transfer from ${this.account1};  Tp ${this.account2}; Amount ${this.amount}; Error ${this.error}`;
+        }
+
+    }
+}
+
+function transfer2(acc1, acc2, amount) {
+    result = acc1.withdraw2(amount);
+    if (result == null) {
+        acc2.deposit(amount);
+        transaction = new Transaction(acc1, acc2, amount);
+    }
+    else
+        transaction = new Transaction(acc1, acc2, amount, result);
+    console.log(transaction);
+
+}
+
+transfer2(account2, account1, 50);
+transfer2(account2, account1, 500);
