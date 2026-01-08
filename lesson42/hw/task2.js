9cc1b8b32667f4456f6249c6e24158c193c1e32a
Yaroslav Benin <dondublon1@yandex.ru>
2026-01-08 15:25:18 +0200

    lesson 42, task 2, beginning.

---
+++ lesson42/hw/task2.js
@@ -0,0 +1,15 @@
+let btnAdd = document.getElementById("btnAdd");
+let inputName = document.getElementById("inputName");
+let inputSirname = document.getElementById("inputSirname");
+let inputEmail = document.getElementById("inputEmail");
+btnAdd.addEventListener("click", addParagraph)
+
+function addParagraph() {
+    let str = `Name: ${inputName.value}, Sirname: ${inputSirname.value}, Email: ${inputEmail.value}`;
+    const newP = document.createElement("p");
+    const newPText = document.createTextNode(str);
+    newP.appendChild(newPText);
+    document.body.appendChild(newP);
+
+}
+
