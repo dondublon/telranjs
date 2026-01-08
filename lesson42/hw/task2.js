56a4c34ff0664af8feb692944d1db59cd9f63ced
Yaroslav Benin <dondublon1@yandex.ru>
2026-01-08 15:31:10 +0200

    lesson 42, task 2, + preparing to delete

--- lesson42/hw/task2.js
+++ lesson42/hw/task2.js
@@ -6,11 +6,14 @@ btnAdd.addEventListener("click", addParagraph)
 
 function addParagraph() {
     event.preventDefault();
+    const wrapper = document.createElement("div");
     let str = `Name: ${inputName.value}, Sirname: ${inputSirname.value}, Email: ${inputEmail.value}`;
     const newP = document.createElement("p");
     const newPText = document.createTextNode(str);
     newP.appendChild(newPText);
-    document.body.appendChild(newP);
+
+    wrapper.appendChild(newP);
+    document.body.appendChild(wrapper);
 
 }
 
